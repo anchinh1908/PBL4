d75400bb21f39c0a041f117b4698175cd4c7b6ed
anchinh <anchinhktvn123@gmail.com>
2023-11-12 17:31:35 +0700

    Aggregation 3 commits Chinh Nhung 12.11.2023

---
+++ SERVER/src/Model/M_DBconnect.java
@@ -0,0 +1,28 @@
+
+package Model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+public class M_DBconnect {
+
+    public static Connection con=null;
+    
+    public static void loadConnection(){
+        String url = "jdbc:mysql://localhost:3309/pbl4";
+        String root = "root";
+        String pass = "123456789";
+                
+        try{
+           con=DriverManager.getConnection(url,root,pass);
+           
+           if(con!=null){
+           System.out.println("Ket noi thanh cong");
+           }
+        } catch(Exception e){
+            System.out.println("Loi");            
+        }
+}
+}
